f191b775502452c61ef7b92f5da36dea951e01d0
Gary Popov <gpopov@fbdfrozen.com>
2021-08-31 18:15:46 -0500

    Add 2019/Day2

---
+++ 2019/day02-2.exs
@@ -0,0 +1,70 @@
+# An Intcode program is a list of integers separated by commas (like 1,0,0,3,99).
+# To run one, start by looking at the first integer (called position 0).
+# Here, you will find an opcode - either 1, 2, or 99. The opcode indicates what to do;
+# For example, 99 means that the program is finished and should immediately halt.
+# Encountering an unknown opcode means something went wrong.
+# Opcode 1 adds together numbers read from two positions and stores the result in a
+# third position. The three integers immediately after the opcode tell you these three
+# positions - the first two indicate the positions from which you should read the input
+# values, and the third indicates the position at which the output should be stored.
+# For example, if your Intcode computer encounters 1,10,20,30, it should read the
+# values at positions 10 and 20, add those values, and then overwrite the value at position 30 with their sum.
+# Opcode 2 works exactly like opcode 1, except it multiplies the two inputs
+# instead of adding them. Again, the three integers after the opcode indicate where
+# the inputs and outputs are, not their values.
+# Once you're done processing an opcode, move to the next one by stepping forward 4 positions.
+
+defmodule IntCodeCPU do
+  def sum(program, position) do
+    result = program[program[position+1]] + program[program[position+2]]
+    dest = program[position+3]
+
+    program = Map.replace(program, dest, result)
+    execute(program, position+4)
+  end
+
+  def prod(program, position) do
+    result = program[program[position+1]] * program[program[position+2]]
+    dest = program[position+3]
+    program = Map.replace(program, dest, result)
+    execute(program, position+4)
+  end
+
+  def halt(program, _) do
+    program[0]
+  end
+
+  def execute(program, position) do
+    case program[position] do
+      1 -> sum(program, position)
+      2 -> prod(program, position)
+      99 -> halt(program, position)
+    end
+  end
+end
+
+program = File.read("day2.txt")
+          |> elem(1)
+          |> String.split(",")
+          |> Enum.map(fn s -> String.to_integer(s) end)
+          |> Enum.with_index()
+          |> Enum.map(fn {k,v} -> {v,k} end )
+          |> Map.new()
+
+
+# Find the input noun and verb that cause the program to produce the
+# output 19690720. What is 100 * noun + verb?
+# (For example, if noun=12 and verb=2, the answer would be 1202.)
+
+verbs = Enum.to_list(1..99)
+nouns = Enum.to_list(1..99)
+Enum.each verbs, fn x ->
+  Enum.each nouns, fn y ->
+    patched_program = Map.replace(program, 1, x)
+    patched_program = Map.replace(patched_program, 2, y)
+    result = IntCodeCPU.execute(patched_program,0)
+    if result == 19690720 do
+      IO.puts("Ouput: 100*#{x}+#{y} = #{100*x+y}")
+    end
+  end
+end
