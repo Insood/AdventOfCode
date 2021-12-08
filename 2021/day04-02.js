4523142462d386c8505c5335abe700c94404337d
Gary Popov <gary.popov@sourceday.com>
2021-12-08 17:12:38 -0600

    2021/Day4

---
+++ 2021/day04-02.js
@@ -0,0 +1,62 @@
+const fs = require('fs');
+
+const input_file = process.argv.slice(2,3)[0];
+
+let winning_positions = [
+  [ 0, 1, 2, 3, 4],
+  [ 5, 6, 7, 8, 9],
+  [10,11,12,13,14],
+  [15,16,17,18,19],
+  [20,21,22,23,24],
+  [ 0, 5,10,15,20],
+  [ 1, 6,11,16,21],
+  [ 2, 7,12,17,22],
+  [ 3, 8,13,18,23],
+  [ 4, 9,14,19,24]
+]
+
+let inputs = fs.readFileSync(input_file, 'utf8')
+               .trim()
+               .split('\n\n')
+
+let moves = inputs[0].split(',').map(el => parseInt(el))
+let boards = inputs.slice(1).map(line => line.trim().replace("\n", ' ').split(/\s+/).map(el => parseInt(el)) )
+
+// deep copy boards
+var score_boards = JSON.parse(JSON.stringify(boards));
+
+function is_winner(board){
+  let winning_position_found = winning_positions.map(positions => {
+    let winner = positions.map( position => board[position] == 'x').filter( value => value).length == 5;
+    return winner;
+  }).filter(winner => winner).length > 0
+  return winning_position_found;
+}
+
+function play(moves, boards){
+  let winning_board_summaries = [];
+
+  moves.forEach((move, move_ix) => {
+    score_boards.forEach((board, board_ix) => {
+      if(winning_board_summaries.find(summary => summary[0] == board_ix)){ return }
+
+      new_board = board.map(val => val == move ? 'x' : val)
+      boards[board_ix] = new_board
+      if(is_winner(new_board)){
+        winning_board_summaries.push([board_ix, move_ix, new_board, move])
+      }
+    });
+  });
+  return winning_board_summaries;
+}
+
+function score(board_summary){
+  let board = board_summary[2];
+  let winning_move = board_summary[3];
+  let winning_score =board.filter(val => val != 'x').reduce((prev, current) => prev + current)*winning_move;
+  return winning_score;
+}
+
+let winning_board_summaries = play(moves, score_boards)
+let last_board = winning_board_summaries.pop();
+console.log(score(last_board));
